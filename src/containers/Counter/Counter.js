2be203727291745f444d4bebe03e3c523cb0da3d
Khoutsaneh Kettavong <laekettavong@Khoutsanehs-iMac.local>
2018-10-09 12:05:03 -0400

    Housekeeping

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -5,32 +5,6 @@ import CounterOutput from '../../components/CounterOutput/CounterOutput';
 import * as Actions from '../../store/actions'
 
 class Counter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            counter: 0
-        }
-    }
-
-    counterChangedHandler = (action, value) => {
-        switch (action) {
-            case 'inc':
-                this.setState((prevState) => { return { counter: prevState.counter + 1 } })
-                break;
-            case 'dec':
-                this.setState((prevState) => { return { counter: prevState.counter - 1 } })
-                break;
-            case 'add':
-                this.setState((prevState) => { return { counter: prevState.counter + value } })
-                break;
-            case 'sub':
-                this.setState((prevState) => { return { counter: prevState.counter - value } })
-                break;
-            default:
-                break;
-        }
-    }
-
     render() {
         return (
             <div>
